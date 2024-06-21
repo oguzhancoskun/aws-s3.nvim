18d183c8b58a2bfd6214f35b6a52cc3428fb770d
oguzhan <svartonn@gmail.com>
2024-06-21 21:48:09 +0100

    fix luacheck errors

--- lua/aws-s3/s3.lua
+++ lua/aws-s3/s3.lua
@@ -28,11 +28,8 @@ end
 
 local function s3_create(remote_path, profile)
   local file_path = get_active_file_path()
-  
   local command = string.format("aws s3 cp %s s3://%s --profile %s", file_path, remote_path, profile)
-  
   local success = os.execute(command)
-  
   if success then
     send_notification("File successfully uploaded to S3: " .. remote_path, vim.log.levels.INFO)
   else
